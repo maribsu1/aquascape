d81d78556717a041623fb60661e262d0b7f8383b
maribsu1 <maribsu2@gmail.com>
2023-08-13 04:38:29 -0500

    proyecto finalizado

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# aquascape
+# Piscinas
+
+Proyecto frontend desarrollado como parte de una prueba técnica para un puesto de desarrollador frontend. Utiliza HTML, CSS y JavaScript para crear una interfaz de usuario elegante y funcional.
+
+## Descripción
+
+Proyecto frontend fue creado como parte de una prueba técnica para un proceso de selección de desarrollador frontend. El objetivo era demostrar habilidades en la creación de interfaces de usuario atractivas y funcionales utilizando tecnologías web estándar. El proyecto incluye una estructura HTML básica, estilos CSS para una apariencia agradable y un archivo JavaScript para posibles interacciones del usuario.
+
+## Autor
+
+Proyecto desarrollado por [Mariangel Socorro](https://github.com/maribsu1).
+
+
---
+++ docs/js/script.js
@@ -0,0 +1,74 @@
+// Función para activar/desactivar la clase 'show' en la barra de navegación
+function toggleNavbar() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('show');
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Función para alternar la clase "show" en la barra de navegación
+    function toggleNavbar() {
+        const navbarNav = document.querySelector('.navbar-nav');
+        if (navbarNav) {
+            navbarNav.classList.toggle('show');
+        }
+    }
+
+    // Agregar evento de clic al icono del toggler
+    const toggler = document.querySelector('.navbar-toggler');
+    if (toggler) {
+        toggler.addEventListener('click', toggleNavbar);
+    }
+
+    // Cambiar el fondo de la barra de navegación al hacer scroll
+    window.addEventListener('scroll', () => {
+        const currentScrollPos = window.pageYOffset;
+        const navbar = document.querySelector('.navbar');
+
+        if (navbar) {
+            if (currentScrollPos === 0) {
+                navbar.classList.remove('scrolled');
+            } else {
+                navbar.classList.add('scrolled');
+            }
+        }
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var backToTopBtn = document.getElementById("back-to-top-btn");
+
+    if (backToTopBtn) {
+        // Mostrar el botón al hacer scroll
+        window.addEventListener("scroll", function () {
+            if (window.scrollY > 200) {
+                backToTopBtn.classList.add("show");
+            } else {
+                backToTopBtn.classList.remove("show");
+            }
+        });
+
+        // Volver al inicio al hacer clic
+        backToTopBtn.addEventListener("click", function (e) {
+            e.preventDefault();
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        });
+    }
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    document.getElementById('contact-form').style.display = 'none';
+    document.getElementById('confirmation-message').style.display = 'block';
+});
+
+    document.addEventListener("DOMContentLoaded", function () {
+        var navbarToggler = document.querySelector(".navbar-toggler");
+        var navbarNav = document.querySelector("#navbarNav");
+
+        navbarToggler.addEventListener("click", function () {
+            navbarNav.classList.toggle("show");
+        });
+    });
+
+    
